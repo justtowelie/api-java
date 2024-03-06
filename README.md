03743de2e2104a253cd15e88bb90c4ca61fc1f07
Daniel Leggett <danielleggett1@gmail.com>
2024-03-06 17:42:08 +0000

    skeleton introduced

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # api-java
 Rest Assured framework using Junit.
 - https://restful-booker.herokuapp.com/apidoc/index.html
-
-
->>>>>>> Stashed changes
--- tests/src/main/java/utils/RestClient.java
+++ tests/src/main/java/utils/RestClient.java
@@ -1,8 +1,26 @@
 package utils;
 
+import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.response.Response;
+
 public class RestClient {
 
 
+    public void setUp(String baseURI)
+    {
+        RestAssured.baseURI = baseURI;
 
+        RestAssured.filters(new RequestLoggingFilter(),
+                new ResponseLoggingFilter());
+    }
+
+    public Response getRequest(String endpoint)
+    {
+        // Perform GET request to /booking endpoint
+        return RestAssured.given()
+                .get(endpoint);
+    }
 
 }
