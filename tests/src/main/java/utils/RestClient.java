940b9679aaf20fe7e77a1a2fd97dd964eecefd56
Daniel Leggett <danielleggett1@gmail.com>
2024-03-11 12:20:57 +0000

    added awaility

--- tests/src/main/java/utils/RestClient.java
+++ tests/src/main/java/utils/RestClient.java
@@ -5,6 +5,13 @@ import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.awaitility.Awaitility;
+
+import java.sql.Time;
+import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RestClient {
@@ -17,8 +24,7 @@ public class RestClient {
                 new ResponseLoggingFilter());
     }
 
-    public void verifySuccessfulStatus(Response response)
-    {
+    public void verifySuccessfulStatus(Response response) {
         assertEquals(200, response.getStatusCode(), "Status code is not as expected");
     }
 
@@ -39,9 +45,14 @@ public class RestClient {
                 .cookie("token", "abc123").header("Authorization", "Basic YWRtaW46cGFzc3dvcmQxMjM=").body(body).put(endpoint);
     }
 
-    public Response deleteRequest(String endpoint)
-    {
+    public Response deleteRequest(String endpoint) {
         return RestAssured.given().contentType(ContentType.JSON).cookie("token", "abc123").header("Authorization", "Basic YWRtaW46cGFzc3dvcmQxMjM=").delete(endpoint);
+    }
+
+    // Method to recall the endpoint until assertion is fulfilled.
+    public void sendGetRequestAndWaitForAssertion(String endpoint, String path, String assertion) {
+        Awaitility.await().atMost(15, TimeUnit.SECONDS).pollInterval(3, TimeUnit.SECONDS).untilAsserted(() -> assertThat(
+                RestAssured.given().get(endpoint).then().contentType(ContentType.JSON).extract().response().body().path(path), is(assertion)));
     }
 
 }
